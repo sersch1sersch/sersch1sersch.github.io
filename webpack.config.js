461426971f6975326af3e71bf4351f12ce5cf318
sersch1sersch <sersch11031984@gmail.com>
2022-05-09 18:48:19 +0300

    ghh

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,33 @@
-const productionConfig = merge([
-    {
-      output: {
-  
-        publicPath: "/",
-  
-  
-        // Tweak this to match your GitHub project name
-        publicPath: "/webpack-demo/",
-  
-      },
+// const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: /* isProduction ? 'none' :  */'source-map',
+    watch: !isProduction,
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'script.js',
     },
-    ...
-  ]);
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+    ],
+  };
+
+  return config;
+};
