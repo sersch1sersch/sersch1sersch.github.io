a1bd73aacc56d3d5c005817a016b59ac0011f8cd
sersch1sersch <sersch11031984@gmail.com>
2022-05-09 18:40:17 +0300

    ghh

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const productionConfig = merge([
+    {
+      output: {
+  
+        publicPath: "/",
+  
+  
+        // Tweak this to match your GitHub project name
+        publicPath: "/webpack-demo/",
+  
+      },
+    },
+    ...
+  ]);
